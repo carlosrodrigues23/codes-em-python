080821bb5c4b46274a3d9f61eb7471f6ef1ecde6
Carlos Gabriel <126592386+carlosrodrigues23@users.noreply.github.com>
2023-04-02 18:12:41 -0300

    Update Calculo de IMC.py

--- Calculo de IMC.py
+++ Calculo de IMC.py
@@ -16,10 +16,10 @@ IMC = float_peso/(float_altura*float_altura)
 print(f'Oi {nome}, seu IMC eh:\n {IMC:.2F}')
 
 if IMC <18.5:
-  print(nome,'você está magro')
+  print('você está magro')
 
-if IMC >=18.5 and IMC <=24.9:
-  print(nome,'você está com peso normal')
+elif IMC >=18.5 and IMC <=24.9:
+  print('você está com peso normal')
 
-if IMC >=25 and IMC <=29.9:
-  print(nome,'você está com sobrepeso')
+else:
+  print('você está com sobrepeso')
